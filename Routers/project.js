dcbfe88792e840fd17a097f6f541fdd8ee556a99
DuvanAndres <mantillatorresduvanandres@gmail.com>
2025-07-22 00:22:02 -0500

    Update project.js

--- Routers/project.js
+++ Routers/project.js
@@ -1,21 +1,15 @@
 import { Router } from "express";
-
-//import http  from "../controllers/.js"
+import * as ProjectController from "../controllers/projectController.js";
 
 const router = Router();
 
-router.get('api/projects')
+router.get('/api/projects', ProjectController.getProjects);
+router.get('/api/projects/:id', ProjectController.getProjectById);
+router.post('/api/projects', ProjectController.createProject);
+router.post('/api/projects/:id/members', ProjectController.addProjectMember);
+router.put('/api/projects/:id', ProjectController.updateProject);
+router.put('/api/projects/:id/status', ProjectController.updateProjectStatus);
+router.delete('/api/projects/:id', ProjectController.deleteProject);
+router.delete('/api/projects/:id/members/:userId', ProjectController.removeProjectMember);
 
-router.get('api/projects/:id')
-
-router.post('api/projects')
-
-router.post('api/projects/:id/members')
-
-router.put('api/projects/:id')
-
-router.put('api/projects/:id/status')
-
-router.delete('api/projects/:id')
-
-router.delete('api/projects/:id/members/:userld')
+export default router;
