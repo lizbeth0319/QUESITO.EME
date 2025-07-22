e6e4fe68fe6dc676808acd4ec41881082e44e0a0
DuvanAndres <mantillatorresduvanandres@gmail.com>
2025-07-22 00:22:53 -0500

    Update role.js

--- Routers/role.js
+++ Routers/role.js
@@ -1,13 +1,11 @@
 import { Router } from "express";
-
-//import http  from "../controllers/.js"
+import * as RoleController from "../controllers/role.js";
 
 const router = Router();
 
-router.get('api/roles')
+router.get('/api/roles', RoleController.getRoles);
+router.post('/api/roles', RoleController.createRole);
+router.put('/api/roles/:id', RoleController.updateRole);
+router.delete('/api/roles/:id', RoleController.deleteRole);
 
-router.post('api/roles')
-
-router.put('api/roles/:id')
-
-router.delete('api/roles/:id')
+export default router;
